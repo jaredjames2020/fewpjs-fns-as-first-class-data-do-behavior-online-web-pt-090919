943eb1c2f0b1d25c287257900ef67c005a7928f4
Jared S <jaredjames2020@gmail.com>
2020-03-30 20:23:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function handleClick(e) {
 function greet(timeString){
   time = parseInt(timeString)
   if (time < 12) return "Good Morning"
-  if (time >= 17) return "Good Evening"
+  if (time > 17) return "Good Evening"
   return "Good Afternoon"
 }
 
