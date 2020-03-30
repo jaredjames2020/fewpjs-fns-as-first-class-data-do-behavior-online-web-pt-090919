a322c0adf562ba9ee07a543feff795dd0a7f2d3b
Jared S <jaredjames2020@gmail.com>
2020-03-30 20:20:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 
+// function greet(timeString){
+//   time = parseInt(timeString)
+//   if (time < 12){
+//     greeting = "Good Morning"
+//   }else if (time >= 12 && time < 17){
+//     greeting = "Good Afternoon"
+//   }else {
+//     greeting = "Good Evening"
+//   }
+//   return greeting;
+// }
+
 function greet(timeString){
   time = parseInt(timeString)
   if (time < 12){
-     greeting = "Good Morning"
+    greeting = "Good Morning"
   }else if (time >= 12 && time < 17){
     greeting = "Good Afternoon"
   }else {
