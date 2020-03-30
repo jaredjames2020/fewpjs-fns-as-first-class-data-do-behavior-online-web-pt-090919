87e3715a3aad8df1539bbf24426e149f30cc8213
Jared S <jaredjames2020@gmail.com>
2020-03-30 20:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 
-// function greet(timeString){
-//   time = parseInt(timeString)
-//   if (time < 12){
-//     greeting = "Good Morning"
-//   }else if (time >= 12 && time < 17){
-//     greeting = "Good Afternoon"
-//   }else {
-//     greeting = "Good Evening"
-//   }
-//   return greeting;
-// }
-
 function greet(timeString){
   time = parseInt(timeString)
-  if (time < 12) return "Good Morning"
-  if (time > 16) return "Good Evening"
-  return "Good Afternoon"
+  if (time < 12){
+    greeting = "Good Morning"
+  }else if (time >= 12 && time < 17){
+    greeting = "Good Afternoon"
+  }else {
+    greeting = "Good Evening"
+  }
+  return greeting;
 }
+
+// function greet(timeString){
+//   time = parseInt(timeString)
+//   if (time < 12) return "Good Morning"
+//   if (time > 16) return "Good Evening"
+//   return "Good Afternoon"
+// }
 
 /* Write your implementation of displayMessage() */
 
