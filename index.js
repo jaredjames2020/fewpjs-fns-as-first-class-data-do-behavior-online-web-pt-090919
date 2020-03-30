37d3d3d709de92462f5123083a036a8f0a2e67de
Jared S <jaredjames2020@gmail.com>
2020-03-30 19:48:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function greet(timeString){
      greeting = "Good Morning"
   }else if (time >= 12 && time < 17){
     greeting = "Good Afternoon"
-  }else if (time >= 17){
+  }else if (time > 17){
     greeting = "Good Evening"
   }else {
     greeting = "Error with time"
