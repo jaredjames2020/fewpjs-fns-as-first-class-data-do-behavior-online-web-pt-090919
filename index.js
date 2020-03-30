0e924e80d19fa79763f2d7341caa4846c8506603
Jared S <jaredjames2020@gmail.com>
2020-03-30 20:13:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function greet(timeString){
 }
 
 /* Write your implementation of displayMessage() */
-function displayMessage(message){
-  document.getElementById("greeting").innerHTML = message;
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg;
 }
